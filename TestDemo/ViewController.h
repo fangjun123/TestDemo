c7fa4aa5b0729d0c27ae88afdb33c8d85ea5b2a1
FANG_JUN <348640102@qq.com>
2016-07-08 09:10:20 +0800

    Initial Commit

---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by bocai on 16/7/8.
+//  Copyright © 2016年 bocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by bocai on 16/7/8.
+//  Copyright © 2016年 bocai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
